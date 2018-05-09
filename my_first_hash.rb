a3e4789365f1c61e375d43177c12973780e75967
Mary-Kate James <marykatj@gmail.com>
2018-05-09 19:44:06 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,12 +24,4 @@ def adding
                  }
 shipping_manifest["muskets" => 2, "gun powder" => 4]
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
 end
