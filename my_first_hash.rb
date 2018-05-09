4b70e53394c30f61a79721c6ddf834256d0c6bb5
Mary-Kate James <marykatj@gmail.com>
2018-05-09 19:43:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+shipping_manifest["muskets" => 2, "gun powder" => 4]
 
 
 
